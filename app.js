ba80e785d3c293bad3f179f54e6641587eb12eee
maiad10 <91215810+maiad10@users.noreply.github.com>
2021-09-23 14:11:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Wassam!'));
+app.get('/', (req, res) => res.send('Wassam!-Hi'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
 
 git add app.js
-git commit -m"change message"
+git commit -m "Full pipeline test"
 
 git push 
